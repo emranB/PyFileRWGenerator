9e237888a8e92d07cf7e9798b9eb111224481c16
Emran Billah <omegaregions0072004@yahoo.com>
2023-02-26 02:09:18 -0400

    Fixing tests

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-"""
-This script should read text from a text file. It should split the text on comma
-and output each resulting string in lowercase with the corresponding line number
-prefixed. The script should also print the total number of lines at the end.
-
-Identify and correct any problems with the code and modify it to be more
-robust and maintainable.
-
-Example usage:
-
-$ python line_numbers.py input.txt output.txt
-Total number of lines in output file: 7
-
-Example input file:
-Beautiful is better than ugly,Explicit is better than implicit,Simple is better than complex,Complex is better than complicated, Flat is better than nested, Sparse is better than dense, Readability counts
-
-Example output file:
-1 beautiful is better than ugly
-2 explicit is better than implicit
-3 simple is better than complex
-4 complex is better than complicated
-5 flat is better than nested
-6 sparse is better than dense
-7 readability counts
-
-Note:
-1. This is NOT a trick question.
-2. Expectation is to write bug-free, clean, robust, maintainable and testable code.
-3. The script should work for small and large files.
-"""
-
 import sys
 from modules.FileHandler import *
 
